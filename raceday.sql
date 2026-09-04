6e02a3f7c5aadf384564bb34472d6213cdc62b39
ST10481773 <ST10481773@rcconnect.edu.za>
2026-09-04 23:06:22 +0200

    Added code for events table

--- raceday.sql
+++ raceday.sql
@@ -18,3 +18,18 @@ CREATE TABLE Users (
     created_at DATETIME NOT NULL CONSTRAINT DF_Users_CreatedAt DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT FK_Users_Roles FOREIGN KEY (role_id) REFERENCES Roles(id) ON DELETE NO ACTION
 );
+
+CREATE TABLE Events (
+    id INT IDENTITY(1,1) CONSTRAINT PK_Events PRIMARY KEY,
+    name VARCHAR(150) NOT NULL,
+    description NVARCHAR(MAX) NULL,
+    date DATETIME NOT NULL,
+    location VARCHAR(255) NOT NULL,
+    distance DECIMAL(5,2) NOT NULL,
+    event_type_id INT NOT NULL,
+    banner_image_url VARCHAR(2048) NULL,
+    organiser_id INT NOT NULL,
+    created_at DATETIME NOT NULL CONSTRAINT DF_Events_CreatedAt DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT FK_Events_EventTypes FOREIGN KEY (event_type_id) REFERENCES EventTypes(id) ON DELETE NO ACTION,
+    CONSTRAINT FK_Events_Users FOREIGN KEY (organiser_id) REFERENCES Users(id) ON DELETE NO ACTION
+);
